cea35afbca030008b403a6bafe35a8902044be20
jude-snyder <judesnyder10@gmail.com>
2026-03-24 14:40:07 +0000

    changed some colors

--- src/draw-frame.ts
+++ src/draw-frame.ts
@@ -14,13 +14,13 @@ console.log(stages[stage]);
 const display = letters.map((l) => (guesses.has(l) ? l : "_")).join(" ");
 
 console.log(chalk.greenBright(`Guess: ${display}`));
-console.log(chalk.red(`Incorrect: ${[...incorrect].sort().join(" ")}`));
+console.log(chalk.blue(`Incorrect: ${[...incorrect].sort().join(" ")}`));
 
 if (stage === stages.length - 1) {
     console.log(chalk.red(`You lose! The word was ${word}`));
     process.exit(0);
 } else if (letters.every((l) => guesses.has(l))) {
-    console.log(chalk.green("You win!"));
+    console.log(chalk.cyan("You win! 🥳🥇🥳"));
     process.exit(0);
 }
 }
